3d2a266f3336cc9e4bd09cccf3ab525babd6041c
igor-couto <igor.fcouto@gmail.com>
2023-05-18 14:52:09 -0300

    fix: fix last line not showing and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-const DECIMALS: [u8; 127] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126];
-const CHARS: [&str; 127] = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "DEL"];
+const ASCII_COUNT: usize = 127;
+const CHARS: [&str; ASCII_COUNT] = ["NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "DEL"];
 
-const LINES: u8 = 16u8;
-const COLUMNS: u8 = 8u8;
+const LINES: usize = 16;
+const COLUMNS: usize = (ASCII_COUNT / LINES) + if ASCII_COUNT % LINES > 0 { 1 } else { 0 };
 
 fn main() {
-
     println!();
-    
-    let mut line_buffer = String::new();
-    let mut current_column = 0u8;
 
-    for current_line in 0u8..LINES -1 {
-        
-        while current_column < COLUMNS {
-            
-            let index = ((current_column * LINES) + current_line) as usize;
-            
-            let ascii_to_print = format!("\t{} {}", DECIMALS[index], CHARS[index]);
-            line_buffer.push_str(&ascii_to_print);
-
-            current_column = current_column + 1;
+    for current_line in 0..LINES {
+        for current_column in 0..COLUMNS {
+            let index = current_line + current_column * LINES;
+            if index < ASCII_COUNT {
+                let ascii_to_print = format!("\t{} {}", index, CHARS[index]);
+                print!("{}", ascii_to_print);
+            }
         }
-
-        println!("{}", line_buffer);
-        line_buffer.clear();
-        current_column = 0u8;
+        println!();
     }
-
     println!();
 }
